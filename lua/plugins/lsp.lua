abfa5c958d46dd7038220592fdf0b06319c8f0c1
Shayan Khodarahimi <i4zaax@gmail.com>
2025-09-26 01:40:43 +0330

    lsp bug fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
--- LSP + completion stack (no lsp-zero)
+-- lua/plugins/lsp.lua
 return {
-  -- mason: manage external tools (LSP servers, formatters, etc.)
+  -- 1) Mason: install LSP servers/binaries
   {
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
@@ -9,7 +9,7 @@ return {
     end,
   },
 
-  -- mason-lspconfig: auto-setup lspconfig from mason installs
+  -- 2) mason-lspconfig: bridge mason <-> lspconfig
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "williamboman/mason.nvim" },
@@ -21,7 +21,7 @@ return {
     end,
   },
 
-  -- lspconfig: actually start LSP servers
+  -- 3) nvim-lspconfig: actually start servers
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -30,10 +30,13 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      -- capabilities: let LSP know we support nvim-cmp
+      local lspconfig = require("lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
+
+      -- capabilities for nvim-cmp completion
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- common on_attach: buffer-local LSP keymaps
+      -- common on_attach keymaps
       local on_attach = function(_, bufnr)
         local map = function(mode, lhs, rhs, desc)
           vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
@@ -41,43 +44,37 @@ return {
         map("n", "gd", vim.lsp.buf.definition, "Go to definition")
         map("n", "gr", vim.lsp.buf.references, "References")
         map("n", "gi", vim.lsp.buf.implementation, "Implementation")
-        map("n", "K",  vim.lsp.buf.hover, "Hover")
+        map("n", "K",  vim.lsp.buf.hover, "Hover docs")
         map("n", "<leader>rn", vim.lsp.buf.rename, "Rename")
         map("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
-        map("n", "<leader>fd", function() vim.diagnostic.open_float(nil, { border = "rounded" }) end, "Line diagnostics")
         map("n", "[d", vim.diagnostic.goto_prev, "Prev diagnostic")
         map("n", "]d", vim.diagnostic.goto_next, "Next diagnostic")
+        map("n", "<leader>fd", function() vim.diagnostic.open_float(nil, { border = "rounded" }) end, "Line diagnostics")
         map("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, "Format")
       end
 
-      -- hook mason-lspconfig to set up installed servers
-      require("mason-lspconfig").setup_handlers({
-        function(server)
-          require("lspconfig")[server].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-          })
-        end,
+      -- Setup all installed servers (works even if setup_handlers is missing)
+      local servers = mason_lspconfig.get_installed_servers()
 
-        -- per-server tweaks (examples)
-        ["lua_ls"] = function()
-          require("lspconfig").lua_ls.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = {
-              Lua = {
-                diagnostics = { globals = { "vim" } },
-                workspace = { checkThirdParty = false },
-                telemetry = { enable = false },
-              },
+      for _, server in ipairs(servers) do
+        local opts = { capabilities = capabilities, on_attach = on_attach }
+
+        if server == "lua_ls" then
+          opts.settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } },
+              workspace = { checkThirdParty = false },
+              telemetry = { enable = false },
             },
-          })
-        end,
-      })
+          }
+        end
+
+        lspconfig[server].setup(opts)
+      end
     end,
   },
 
-  -- nvim-cmp (completion) + snippets
+  -- 4) nvim-cmp + snippets
   {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -90,35 +87,21 @@ return {
       "rafamadriz/friendly-snippets",
     },
     config = function()
-      -- load VSCode-style snippets
       require("luasnip.loaders.from_vscode").lazy_load()
-
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
+      local cmp, luasnip = require("cmp"), require("luasnip")
       cmp.setup({
-        snippet = {
-          expand = function(args) luasnip.lsp_expand(args.body) end,
-        },
+        snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
         mapping = cmp.mapping.preset.insert({
-          ["<CR>"]   = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"]  = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
+          ["<CR>"]  = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
+            else fallback() end
           end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
+            if cmp.visible() then cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then luasnip.jump(-1)
+            else fallback() end
           end, { "i", "s" }),
         }),
         sources = {
