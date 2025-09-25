0e7d5b6217f8214918ce3dcafba5876e72f2619d
4zaax <i4zaax@gmail.com>
2025-09-25 21:52:25 +0000

    undotree added

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,7 +6,7 @@ return {
         local mark = require("harpoon.mark")
         local ui = require ("harpoon.ui")
         vim.keymap.set("n","<leader>a",mark.add_file, {desc = "Harpoon add file"})
-        vim.keymap.set("n","<C-i>", ui.toggle_quick_menu, {desk = "Harpoon menu"})
+        vim.keymap.set("n","<C-i>", ui.toggle_quick_menu, {desc = "Harpoon menu"})
         vim.keymap.set("n","<C-h>", function() ui.nav_file(1) end)
         vim.keymap.set("n","<C-t>", function() ui.nav_file(2) end)
         vim.keymap.set("n","<C-n>", function() ui.nav_file(3) end)
